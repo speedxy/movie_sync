6c58dc53e3849c42a8cb75d0d1d1674f4fd0a516
Eike Swat <eike@swat.zone>
2016-11-20 13:07:09 +0100

    Fied path error

--- read-server.sh
+++ read-server.sh
@@ -1,7 +1,9 @@
+SCRIPT_DIR=$(dirname "$0")
+
 echo "" > movies-serverlist
 #find /mnt/Filme -maxdepth 1 -type d -printf '%f\n' > movies-serverlist
-rsync --exclude=".*" --exclude=".*/" --list-only --password-file ~/.kodi/sync/rsync.pwd rehberg@192.168.123.1::Filme/ | sed 's/^.\{,46\}//' > movies-serverlist
+rsync --exclude=".*" --exclude=".*/" --list-only --password-file ~/.kodi/sync/rsync.pwd rehberg@192.168.123.1::Filme/ | sed 's/^.\{,46\}//' > ${SCRIPT_DIR}/movies-serverlist
 
 echo "" > tv-serverlist
 #find /mnt/Serien -maxdepth 1 -type d -printf '%f\n' > tv-serverlist
-rsync --exclude=".*" --exclude=".*/" --list-only --password-file .~/.kodi/sync/rsync.pwd rehberg@192.168.123.1::Serien/ | sed 's/^.\{,46\}//' > tv-serverlist
+rsync --exclude=".*" --exclude=".*/" --list-only --password-file ~/.kodi/sync/rsync.pwd rehberg@192.168.123.1::Serien/ | sed 's/^.\{,46\}//' > ${SCRIPT_DIR}/tv-serverlist
